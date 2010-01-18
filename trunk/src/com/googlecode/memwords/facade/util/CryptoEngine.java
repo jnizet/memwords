b1ed05d257faa922acc40b909315499fc313b570
jnizet <jnizet@f64f212e-00f7-11df-95d2-51bf8322dccd>
2010-01-18 00:17:54 +0000

    adapted the key size to the appengine restrictions

--- trunk/src/com/googlecode/memwords/facade/util/CryptoEngine.java
+++ trunk/src/com/googlecode/memwords/facade/util/CryptoEngine.java
@@ -8,9 +8,9 @@ import com.google.inject.ImplementedBy;
 public interface CryptoEngine {
 	/**
 	 * Encrypts the given data, using the given key. The algorithm used is AES, with a 
-	 * 256-bits key
+	 * 128-bits key
 	 * @param data the data to encrypt
-	 * @param key the key which must contain 32 bytes
+	 * @param key the key which must contain 16 bytes
 	 * @return the encrypted data or <code>null</code> if data is <code>null</code>
 	 */
 	byte[] encrypt(byte[] data, SecretKey key);
@@ -19,7 +19,7 @@ public interface CryptoEngine {
 	 * Decrypts the given data, using the given key. The algorithm used is AES, with a 
 	 * 256-bits key
 	 * @param data the data to decrypt
-	 * @param key the key which must contain 32 bytes
+	 * @param key the key which must contain 16 bytes
 	 * @return the decrypted data or <code>null</code> if data is <code>null</code>
 	 */
 	byte[] decrypt(byte[] data, SecretKey key);
@@ -29,7 +29,7 @@ public interface CryptoEngine {
 	 * 256-bits key
 	 * @param data the data to encrypt, as a String. It's transformed into a byte array 
 	 * using the UTF-8 encoding
-	 * @param key the key which must contain 32 bytes
+	 * @param key the key which must contain 16 bytes
 	 * @return the encrypted data, or <code>null</code> if data is <code>null</code>
 	 */
 	byte[] encryptString(String data, SecretKey key);
@@ -38,7 +38,7 @@ public interface CryptoEngine {
 	 * Decrypts the given data, using the given key. The algorithm used is AES, with a 
 	 * 256-bits key
 	 * @param data the data to decrypt
-	 * @param key the key which must contain 32 bytes
+	 * @param key the key which must contain 16 bytes
 	 * @return the decrypted data as a String, or <code>null</code> if the data is <code>null</code>. 
 	 * It's transformed into a String using the UTF-8 encoding
 	 */
@@ -52,14 +52,15 @@ public interface CryptoEngine {
 	byte[] hash(byte[] data);
 	
 	/**
-	 * Transforms the raw byte array into a secret key for the AES algorithm
-	 * @param keyAsBytes the key bytes
+	 * Transforms the raw byte array into a secret key for the AES (128 bits - 16 bytes) algorithm
+	 * @param keyAsBytes the key bytes, which must contain at least 16 bytes (only the 16 first 
+	 * bytes are kept)
 	 * @return the key as a <code>SecretKey</code>
 	 */
 	SecretKey bytesToSecretKey(byte[] keyAsBytes);
 	
 	/**
-	 * Generates a random encryption key for the AES algorithm
+	 * Generates a random encryption key for the AES (128 bits - 16 bytes) algorithm
 	 */
 	SecretKey generateEncryptionKey();
 }
