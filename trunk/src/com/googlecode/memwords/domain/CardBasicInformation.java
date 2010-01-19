f83f614c81a47cbaf3edd6fc7b6fa0f21914df70
jnizet <jnizet@f64f212e-00f7-11df-95d2-51bf8322dccd>
2010-01-19 16:09:17 +0000

    improved code

--- trunk/src/com/googlecode/memwords/domain/CardBasicInformation.java
+++ trunk/src/com/googlecode/memwords/domain/CardBasicInformation.java
@@ -3,16 +3,15 @@ package com.googlecode.memwords.domain;
 import java.io.Serializable;
 
 /**
- * Basic information about a card, in  order to display a card list. Instances of this class
- * have a natural order : thee natural order of their name.
+ * Basic information about a card, in  order to display a card list.
  * @author JB
  */
 @SuppressWarnings("serial")
-public final class CardBasicInformation implements Serializable, Comparable<CardBasicInformation> {
+public final class CardBasicInformation implements Serializable {
     private String id;
     private String name;
     private String iconUrl;
-    
+
     public CardBasicInformation(String id, String name, String iconUrl) {
         this.id = id;
         this.name = name;
@@ -29,10 +28,5 @@ public final class CardBasicInformation implements Serializable, Comparable<Card
 
     public String getIconUrl() {
         return iconUrl;
-    }
-    
-    @Override
-    public int compareTo(CardBasicInformation o) {
-        return this.name.compareTo(o.name);
     }
 }
