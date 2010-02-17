dcc66e31444a5a540f5f780936a02ba3c72cdac5
JB Nizet <jnizet@users.noreply.github.com>
2010-02-17 11:08:20 +0000

    improved build and clean of minified JS files; improved auto-adjust of sidebar height; stricter JSLint options

--- war/js/main.js
+++ war/js/main.js
@@ -402,14 +402,30 @@ m.install = function() {
         });
         
         function adjustSideBarHeight() {
+            var sideBar = $("#sidebar");
+            // if the original height of the side bar hasn't been stored yet, store it
+            if (!sideBar.data("originalHeight")) {
+                sideBar.data("originalHeight", sideBar.height());
+            }
             var bodyHeight = $("body").outerHeight();
             var viewportHeight = $(window).height();
-            if (bodyHeight < viewportHeight) {
-                var sideBar = $("#sidebar");
-                var newMinHeight = (sideBar.height() + (viewportHeight - bodyHeight)) + "px";
-                sideBar.css("min-height", newMinHeight);
+            // if body height isn't the same as the viewport height, we adjust the size of the sidebar
+            if (bodyHeight != viewportHeight) {
+                // the container is perhaps bigger than the side bar. In this case, it's the container's height
+                // that constitutes the perceived height of the side bar.
+                var sideBarHeight = sideBar.height();
+                var containerHeight = $("#container").height();
+                var currentHeight = sideBarHeight > containerHeight ? sideBarHeight : containerHeight;
+                // we set the height of the side bar so that it's at least equal to its original height,
+                // ant so that the body fills the viewport 
+                var newHeight = (currentHeight + (viewportHeight - bodyHeight));
+                if (newHeight < sideBar.data("originalHeight")) {
+                    newHeight = sideBar.data("originalHeight");
+                }
+                sideBar.css("height", newHeight);
             }
-        };
+        }
+        
         adjustSideBarHeight();
         $(window).resize(adjustSideBarHeight);
     });
--- war/sitemesh/main.jsp
+++ war/sitemesh/main.jsp
@@ -19,7 +19,7 @@
     <script type="text/javascript" src="<c:url value="/js/jquery-1.3.2.js" />"></script>
     <script type="text/javascript" src="<c:url value="/js/jquery-ui-1.7.2.custom.min.js"/>"></script>
     <script type="text/javascript" src="<c:url value="/js/jquery.form.min.js" />"></script>
-    <script type="text/javascript" src="<c:url value="/js/main.js" />"></script>
+    <script type="text/javascript" src="<c:url value="/js/main.min.js" />"></script>
     <script type="text/javascript">
       <c:if test="${header.ajaxSync == true}">$.ajaxSetup({ async: false });</c:if>
       m.config.baseUrl = '${request.contextPath}';
